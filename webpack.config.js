3a473d0ba716fa6b4daa597d06347ec46f72f7d3
lalit <lalit.bansal@io-media.com>
2018-05-28 15:24:26 +0530

    Initial commit Added service worker .

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "./", 'index.html'),
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    mode: "development",
+    entry: ["./index.html"],
+    output: {
+        filename: "bundle.js"
+    },
+    devServer: {
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+
+
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: "/images"
+                }
+            },
+
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            }
+
+
+        ]
+    },
+    plugins: [HTMLWebpackPluginConfig]
+}
