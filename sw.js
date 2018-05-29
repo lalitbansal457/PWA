b498bb4b945b323267ce89a5db688b3a204775bc
lalit <lalit.bansal@io-media.com>
2018-05-29 12:10:43 +0530

    sw changes

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 
 var CACHE_VERSION = 'app-v8';
 var CACHE_FILES = [
+    '/',
+    '/PWA',
     'css/style.css',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
     //'manifest.json'
